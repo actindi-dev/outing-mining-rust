fc76992b428f79f324fd09519846b6a24c3d1bf9
TAHARA Yoshinori <read.eval.print@gmail.com>
2017-03-02 11:07:21 +0900

    refs #25159 いこーよ: アプリのログイン監視
    グラフにする

--- public/js/watch-login.js
+++ public/js/watch-login.js
@@ -67,18 +67,29 @@ $(function() {
     var labels = $.map(chardData, function(x) { return x.date; });
     var failedIpCounts = $.map(chardData, function(x) {
         var n = 0;
-        for(key in x.failed) { if(x.failed.hasOwnProperty(key)) ++n; }
+        $.map(x.failed, function() { ++n });
+        return n;
+    });
+    var failedCounts = $.map(chardData, function(x) {
+        var n = 0;
+        $.map(x.failed, function(v) { n += v });
         return n;
     });
     var myChart = new Chart(ctx, {
-        type: 'bar',
+        //type: 'bar',
+        type: 'line',
         data: {
             labels: labels,
-            datasets: [{
-                label: 'ログイン失敗IP数',
-                data: failedIpCounts,
-                borderWidth: 1
-            }]
+            datasets: [
+                { label: 'ログイン失敗IP数',
+                  data: failedIpCounts,
+                  borderWidth: 1
+                },
+                { label: 'ログイン失敗数',
+                  data: failedCounts,
+                  borderWidth: 1
+                }
+            ]
         },
         options: {
             scales: {
